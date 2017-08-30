ca9fb7f45a8c3e64eb2f0cf46c00a6b35c054675
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-30 12:37:36 -0500

    added set-up code

--- DevUCSharpCS-ASP_ 027/Default.aspx
+++ DevUCSharpCS-ASP_ 027/Default.aspx
@@ -9,6 +9,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
         </div>
     </form>
 </body>
--- DevUCSharpCS-ASP_ 027/Default.aspx.cs
+++ DevUCSharpCS-ASP_ 027/Default.aspx.cs
@@ -11,7 +11,33 @@ namespace DevUCSharpCS_ASP__027
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Random random = new Random();
 
+            int heroHealth = 30;
+            int monsterHealth = 45;
+
+            string result = "";
+
+            //Our intrepid hero gets a bonus first attack...
+            monsterHealth -= random.Next(1, 100);
+
+            int round = 0;
+            result += "<br />Round: " + round;
+            result += String.Format("<br />Hero attacks first, leaving monster with {0} health.",
+                monsterHealth);
+
+            //need battle logic here
+
+            if (heroHealth > 0)
+            {
+                result += "<br />Huzzah! Our hero wins the day!";
+            }
+            else
+            {
+                result += "<br />You know why I like this world? Because in this world, " +
+                    "the bad guys can win!";
+            }
+            resultLabel.Text = result;
         }
     }
 }
